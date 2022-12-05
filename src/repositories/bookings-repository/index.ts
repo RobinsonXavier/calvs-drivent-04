7a087fff08f034deaea661173a38c22a71e0c1c0
RobinsonXavier <robinson.webzen@gmail.com>
2022-12-04 22:36:17 -0300

    error handling for get route added, new functions for booking repository added

--- src/repositories/bookings-repository/index.ts
+++ src/repositories/bookings-repository/index.ts
@@ -1,6 +1,9 @@
 import { prisma } from "@/config";
+import { Booking, Room } from "@prisma/client";
 
-async function findBookingByUserId(userId: number) {
+async function findBookingByUserId(userId: number): Promise<Booking & {
+  Room: Room;
+}> {
   return prisma.booking.findFirst({
     where: {
       userId
@@ -11,8 +14,32 @@ async function findBookingByUserId(userId: number) {
   });
 }
 
+async function createBooking(userId: number, roomId: number): Promise<Booking> {
+  return prisma.booking.create( {
+    data: {
+      userId,
+      roomId
+    }
+  });
+}
+
+async function updateBookingByRoomId(roomId: number, newRoomId: number) {
+  return prisma.booking.update(
+    {
+      where: {
+        id: roomId
+      },
+      data: {
+        roomId: newRoomId
+      }
+    }
+  );
+}
+
 const bookingRepository = {
-  findBookingByUserId
+  findBookingByUserId,
+  createBooking,
+  updateBookingByRoomId
 };
 
 export default bookingRepository;
--- src/services/bookings-service/index.ts
+++ src/services/bookings-service/index.ts
@@ -1,6 +1,21 @@
+import { notFoundError } from "@/errors";
 import bookingRepository from "@/repositories/bookings-repository";
+import enrollmentRepository from "@/repositories/enrollment-repository";
+import ticketRepository from "@/repositories/ticket-repository";
 
 async function getBooking(userId: number) {
+  const checkEnrollment = await enrollmentRepository.findWithAddressByUserId(userId);
+
+  if (!checkEnrollment) {
+    throw notFoundError();
+  }
+
+  const checkTicket = await ticketRepository.findTicketByEnrollmentId(checkEnrollment.id);
+
+  if (!checkTicket || checkTicket.status !== "PAID") {
+    throw notFoundError();
+  }
+
   const result = await bookingRepository.findBookingByUserId(userId);
 
   const finalResult = {
